74484d3477e45f4fc53ab7d3248381ce446b8f9a
Loreta Korf <kofekorf@gmail.com>
2023-02-01 21:51:13 +0200

    myfirstreact

--- vytauto/CodeAcademy-stopwatch/src/main.js
+++ vytauto/CodeAcademy-stopwatch/src/main.js
@@ -1,4 +1,6 @@
-import Timer from "./timer.js"
+import Timer from "./timer.js";
+import"../assets/normalize.css";
+import"../assets/styles.css";
 
 const timerContainer = document.querySelector("#timer-container");
 const addTimerButton = document.querySelector("#btn-add-timer");
---
+++ vytauto/my-first-app/src/components/TodoForm.jsx
@@ -0,0 +1,30 @@
+const formStyle = {
+  display: "flex",
+  height: "45px",
+};
+
+const inputStyle = {
+  borderRadius: 0,
+  border: 0,
+  backgroundColor: "#eee",
+  padding: "0.5rem",
+};
+const buttonStyle = {
+  borderRadius: 0,
+  border: 0,
+  backgroundColor: "#333",
+  padding: "0.5rem",
+  fontWeight: "bold",
+  color: "#eee",
+};
+const TodoForm = () => {
+  return (
+    <div>
+      <div style={formStyle}>
+        <input style={inputStyle} />
+        <button style={buttonStyle}>Add Todo</button>
+      </div>
+    </div>
+  );
+};
+export default TodoForm;
